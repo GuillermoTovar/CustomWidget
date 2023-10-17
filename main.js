7034dc83d56fa97262aef8201719e016b0960e1f
DOAGenesys <119416710+DOAGenesys@users.noreply.github.com>
2023-10-17 13:37:33 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,51 +1,47 @@
 import WebSocketHandler from './websocketHandler.js';
 
-let wsHandler = null;
+let chatWebSocket;
 
-// Initialize WebSocket handler and set up chat events
-async function initializeChat() {
-    const config = await getConfig();
-    const GCMessagingDeplId = config.GCMessagingDeplId;
-    const GCWSSEndpoint = config.GCWSSEndpoint;
-
-    wsHandler = new WebSocketHandler(GCMessagingDeplId, GCWSSEndpoint);
-    wsHandler.connect();
-
-    document.getElementById('send-button').addEventListener('click', sendMessageToServer);
-}
-
-// Send user's message to the server
-function sendMessageToServer() {
-    const inputElem = document.getElementById('chat-input');
-    const message = inputElem.value;
-
-    if (message.trim() !== '') {
-        wsHandler.sendMessage(message);
-        displayMessage('You', message);
-        inputElem.value = '';  // Clear the input field
+document.getElementById('send-button').addEventListener('click', () => {
+    const chatInput = document.getElementById('chat-input');
+    const message = chatInput.value.trim();
+    if (message) {
+        chatWebSocket.sendMessage(message);
+        // Might also want to display the sent message in your chat window here
+        displaySentMessage(message);
+        chatInput.value = ''; // Clear the input field
     }
-}
+});
 
-// Display messages in the chat window
-function displayMessage(sender, message) {
+function displaySentMessage(message) {
     const chatWindow = document.getElementById('chat-window');
-    const messageElem = document.createElement('div');
-    messageElem.classList.add('chat-message');
-    messageElem.innerHTML = `<strong>${sender}:</strong> ${message}`;
-    chatWindow.appendChild(messageElem);
+    const messageElement = document.createElement('div');
+    messageElement.classList.add('sent-message'); // Assuming there is a CSS class for styling sent messages
+    messageElement.textContent = message;
+    chatWindow.appendChild(messageElement);
+    chatWindow.scrollTop = chatWindow.scrollHeight; // Auto scroll to the bottom
 }
 
-// Fetch configuration from server
-async function getConfig() {
-    try {
-        const response = await fetch('/api/getConfig');
-        if (!response.ok) {
-            throw new Error('Environment vars could not be retrieved');
-        }
-        return await response.json();
-    } catch (error) {
-        console.error(error);
-    }
+function displayReceivedMessage(message) {
+    const chatWindow = document.getElementById('chat-window');
+    const messageElement = document.createElement('div');
+    messageElement.classList.add('received-message'); // Assuming there is e a CSS class for styling received messages
+    messageElement.textContent = message;
+    chatWindow.appendChild(messageElement);
+    chatWindow.scrollTop = chatWindow.scrollHeight; // Auto scroll to the bottom
 }
 
-initializeChat();
+// Connect to WebSocket when the configuration is loaded
+async function initializeWebSocket(endpoint, deploymentId) {
+    chatWebSocket = new WebSocketHandler(endpoint, deploymentId);
+    chatWebSocket.connect();
+
+    // TODO: Extend the WebSocketHandler class to handle incoming messages and call displayReceivedMessage
+    // For example, modify the onmessage handler in WebSocketHandler to trigger an event or callback.
+}
+
+// Listen to the custom event to initialize the WebSocket connection
+document.addEventListener('configLoaded', (e) => {
+    const { GCWSSEndpoint, GCMessagingDeplId } = e.detail;
+    initializeWebSocket(GCWSSEndpoint, GCMessagingDeplId);
+});
