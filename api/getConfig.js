3559d781ecdb03e93f2f4bee3d8033c8be243fee
DOAGenesys <119416710+DOAGenesys@users.noreply.github.com>
2023-10-17 12:56:50 +0200

    Update getConfig.js

--- api/getConfig.js
+++ api/getConfig.js
@@ -2,7 +2,8 @@
 module.exports = (req, res) => {
     res.json({
         GCDomain: process.env.GC_DOMAIN,
-        GCMessagingDeplId: process.env.GC_MESSAGING_DEPLOYMENT_ID,
+        GCEnvironment: process.env.GC_ENVIRONMENT,
+        GCMessagingDeplId: process.env.GC_MESSAGING_DEPLOYMENT_ID,       
         directToWM: process.env.DIRECT_TO_WM
     });
 };
