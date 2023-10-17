82b665c099d9b91c205ce07dcdb66301a19401c7
DOAGenesys <119416710+DOAGenesys@users.noreply.github.com>
2023-10-17 16:59:38 +0200

    Update main.js

--- main.js
+++ main.js
@@ -7,41 +7,79 @@ document.getElementById('send-button').addEventListener('click', () => {
     const message = chatInput.value.trim();
     if (message) {
         chatWebSocket.sendMessage(message);
-        // Might also want to display the sent message in your chat window here
         displaySentMessage(message);
-        chatInput.value = ''; // Clear the input field
+        chatInput.value = '';
     }
 });
 
 function displaySentMessage(message) {
     const chatWindow = document.getElementById('chat-window');
     const messageElement = document.createElement('div');
-    messageElement.classList.add('sent-message'); // Assuming there is a CSS class for styling sent messages
+    messageElement.classList.add('sent-message');
     messageElement.textContent = message;
     chatWindow.appendChild(messageElement);
-    chatWindow.scrollTop = chatWindow.scrollHeight; // Auto scroll to the bottom
+    chatWindow.scrollTop = chatWindow.scrollHeight;
 }
 
 function displayReceivedMessage(message) {
     const chatWindow = document.getElementById('chat-window');
     const messageElement = document.createElement('div');
-    messageElement.classList.add('received-message'); // Assuming there is e a CSS class for styling received messages
+    messageElement.classList.add('received-message');
     messageElement.textContent = message;
     chatWindow.appendChild(messageElement);
-    chatWindow.scrollTop = chatWindow.scrollHeight; // Auto scroll to the bottom
+    chatWindow.scrollTop = chatWindow.scrollHeight;
 }
 
-// Connect to WebSocket when the configuration is loaded
+function displayErrorMessage(message) {
+    const chatWindow = document.getElementById('chat-window');
+    const messageElement = document.createElement('div');
+    messageElement.classList.add('error-message');
+    messageElement.textContent = message;
+    chatWindow.appendChild(messageElement);
+    chatWindow.scrollTop = chatWindow.scrollHeight;
+}
+
 async function initializeWebSocket(endpoint, deploymentId) {
     chatWebSocket = new WebSocketHandler(endpoint, deploymentId);
     chatWebSocket.connect();
-
-    // TODO: Extend the WebSocketHandler class to handle incoming messages and call displayReceivedMessage
-    // For example, modify the onmessage handler in WebSocketHandler to trigger an event or callback.
 }
 
-// Listen to the custom event to initialize the WebSocket connection
 document.addEventListener('configLoaded', (e) => {
     const { GCWSSEndpoint, GCMessagingDeplId } = e.detail;
     initializeWebSocket(GCWSSEndpoint, GCMessagingDeplId);
+    fetchMessageHistory().then(messages => {
+        messages.forEach(message => {
+            if (message.direction === "Outbound") {
+                displaySentMessage(message.text);
+            } else {
+                displayReceivedMessage(message.text);
+            }
+        });
+    });
 });
+
+document.addEventListener('errorMessage', (e) => {
+    displayErrorMessage(e.detail);
+});
+
+async function fetchMessageHistory() {
+    // This is a mock. Replace with actual fetch to your API.
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve([
+                {
+                    id: "750fabf734de0ebbd6a53458cbd7848c",
+                    type: "Text",
+                    text: "Hello, how may I help you?",
+                    direction: "Outbound"
+                },
+                {
+                    id: "871ef990-686a-4ebc-a5b8-17a17cf8dbf8",
+                    type: "Text",
+                    text: "Good morning.",
+                    direction: "Inbound"
+                }
+            ]);
+        }, 1000);
+    });
+}
