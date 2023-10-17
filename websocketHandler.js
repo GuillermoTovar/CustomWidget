e376520ccc513fd2c72c196978bb8218f275bb9c
DOAGenesys <119416710+DOAGenesys@users.noreply.github.com>
2023-10-17 16:59:25 +0200

    Update websocketHandler.js

--- websocketHandler.js
+++ websocketHandler.js
@@ -15,7 +15,10 @@ class WebSocketHandler {
 
         this.socket.onmessage = (event) => {
             const data = JSON.parse(event.data);
-            // TODO: Handle incoming messages and update UI
+            if (data.type === "response" && data.code !== 200) {
+                this._handleError(data);
+            }
+            // TODO: Handle other incoming messages and update UI
         };
 
         this.socket.onerror = (error) => {
@@ -30,6 +33,30 @@ class WebSocketHandler {
                 console.error('Connection died');
             }
         };
+
+        setInterval(() => {
+            this._sendEcho();
+        }, 60000); // Send echo every 60 seconds
+    }
+
+    _handleError(data) {
+        let errorMessage;
+        switch (data.code) {
+            case 400:
+                errorMessage = "Bad request. Please check the data you're sending.";
+                break;
+            case 403:
+                errorMessage = "Forbidden. Please check feature toggle or configuration.";
+                break;
+            case 404:
+                errorMessage = "Object not found. Please provide valid ID.";
+                break;
+            // ... (handle other error codes similarly)
+            default:
+                errorMessage = data.body; // Default to the error message from the server
+        }
+        const errorEvent = new CustomEvent('errorMessage', { detail: errorMessage });
+        document.dispatchEvent(errorEvent);
     }
 
     _configureSession() {
@@ -53,8 +80,18 @@ class WebSocketHandler {
         this.socket.send(JSON.stringify(messagePayload));
     }
 
+    _sendEcho() {
+        const echoPayload = {
+            action: "echo",
+            message: {
+                type: "Text",
+                text: "ping"
+            }
+        };
+        this.socket.send(JSON.stringify(echoPayload));
+    }
+
     _generateUUID() {
-        // Simple function to generate a UUID
         return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
             const r = Math.random() * 16 | 0;
             const v = c === 'x' ? r : (r & 0x3 | 0x8);
