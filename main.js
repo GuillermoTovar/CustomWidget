46b66f3de119bcd1832eca65371ad2aa8891868b
DOAGenesys <119416710+DOAGenesys@users.noreply.github.com>
2023-10-17 17:45:02 +0200

    Update main.js

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ document.getElementById('send-button').addEventListener('click', () => {
     if (message) {
         chatWebSocket.sendMessage(message);
         displaySentMessage(message);
-        chatInput.value = '';
+        chatInput.value = ''; // Clear the input field
     }
 });
 
@@ -30,16 +30,7 @@ function displayReceivedMessage(message) {
     chatWindow.scrollTop = chatWindow.scrollHeight;
 }
 
-function displayErrorMessage(message) {
-    const chatWindow = document.getElementById('chat-window');
-    const messageElement = document.createElement('div');
-    messageElement.classList.add('error-message');
-    messageElement.textContent = message;
-    chatWindow.appendChild(messageElement);
-    chatWindow.scrollTop = chatWindow.scrollHeight;
-}
-
-async function initializeWebSocket(endpoint, deploymentId) {
+function initializeWebSocket(endpoint, deploymentId) {
     chatWebSocket = new WebSocketHandler(endpoint, deploymentId);
     chatWebSocket.connect();
 }
@@ -47,43 +38,33 @@ async function initializeWebSocket(endpoint, deploymentId) {
 document.addEventListener('configLoaded', (e) => {
     const { GCWSSEndpoint, GCMessagingDeplId } = e.detail;
     initializeWebSocket(GCWSSEndpoint, GCMessagingDeplId);
-    fetchMessageHistory().then(messages => {
-        messages.forEach(message => {
-            if (message.direction === "Outbound") {
-                displaySentMessage(message.text);
-            } else {
-                displayReceivedMessage(message.text);
-            }
-        });
-    });
 });
 
-document.addEventListener('errorMessage', (e) => {
-    displayErrorMessage(e.detail);
-});
+// To load message history on start, finish the code regarding this part in websocketHandler.js and uncomment below:
+/*
 
-async getJWT() {
-    const messagePayload = {
-        action: "getJwt",
-        token: this.token
-    };
-    const response = await this.sendMessageWithResponse(messagePayload);
-    return response.jwt;
-}
 
-async fetchMessageHistory(jwt, domain, pageSize = 10, pageNumber = 1) {
-    const apiUrl = `/api/v2/webmessaging/messages?pageSize=${pageSize}&pageNumber=${pageNumber}`;
-    const headers = new Headers();
-    headers.append("Authorization", `Bearer ${jwt}`);
-    headers.append("Origin", domain);
-    
-    const response = await fetch(apiUrl, {
-        method: 'GET',
-        headers: headers
-    });
-
-    if (!response.ok) {
-        throw new Error('Failed to fetch message history');
+async function loadMessageHistory() {
+    try {
+        const jwt = await chatWebSocket.getJWT();
+        const messageHistory = await chatWebSocket.fetchMessageHistory(jwt, window.location.origin);
+        displayMessageHistory(messageHistory.entities);
+    } catch (error) {
+        console.error('Failed to load message history:', error);
     }
-    return await response.json();
 }
+
+function displayMessageHistory(messages) {
+    messages.forEach(message => {
+        if (message.direction === "Outbound") {
+            displaySentMessage(message.text);
+        } else {
+            displayReceivedMessage(message.text);
+        }
+    });
+}
+
+// Call this function after the WebSocket is connected and the chat UI is initialized
+// loadMessageHistory();
+
+*/
